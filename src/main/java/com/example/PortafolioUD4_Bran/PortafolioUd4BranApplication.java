63df2d8e20d27c65130540c671259d6a95479eb3
Bran <bran.bouzas.prado@iessanmamede.com>
2025-01-27 16:39:55 +0100

    segundo commit

--- src/main/java/com/example/PortafolioUD4_Bran/PortafolioUd4BranApplication.java
+++ src/main/java/com/example/PortafolioUD4_Bran/PortafolioUd4BranApplication.java
@@ -2,9 +2,10 @@ package com.example.PortafolioUD4_Bran;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class PortafolioUd4BranApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PortafolioUd4BranApplication.class, args);
